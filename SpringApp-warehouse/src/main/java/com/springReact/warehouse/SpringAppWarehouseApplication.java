9c4d49f1fa3c03e242a4ae863f8d9aeb84de9010
irmantasdv <irmantas.dv@gmail.com>
2020-03-03 18:05:58 +0200

    first commit

---
+++ SpringApp-warehouse/src/main/java/com/springReact/warehouse/SpringAppWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.springReact.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppWarehouseApplication.class, args);
+	}
+
+}
---
+++ SpringApp-warehouse/src/test/java/com/springReact/warehouse/SpringAppWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springReact.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
