448e49bdb53abd305fc6858527215a1ffb92275e
irmantasdv <irmantas.dv@gmail.com>
2020-03-06 17:10:06 +0200

    added Category hardcoded test data

--- SpringApp-warehouse/src/main/java/com/springReact/warehouse/SpringAppWarehouseApplication.java
+++ SpringApp-warehouse/src/main/java/com/springReact/warehouse/SpringAppWarehouseApplication.java
@@ -1,12 +1,15 @@
 package com.springReact.warehouse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.springReact.warehouse.ProductRepository.ProductRepository;
-import com.springReact.warehouse.product.ProductModel;
+import com.springReact.warehouse.model.Category;
+import com.springReact.warehouse.model.ProductModel;
+import com.springReact.warehouse.repository.CategoryRepository;
+import com.springReact.warehouse.repository.ProductRepository;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,12 +22,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 public class SpringAppWarehouseApplication {
 
+	@Autowired
+	private ProductRepository productRepository;
+	@Autowired
+	private CategoryRepository categoryRepository;
 	@Bean
-	public CommandLineRunner demo(ProductRepository productRepository) {
+	public CommandLineRunner demo() {
 		return (args) -> {
-			productRepository.save(new ProductModel("A234","House1", 12));
-			productRepository.save(new ProductModel("C234","House3", 56));
-			productRepository.save(new ProductModel("D3453","House4", 72));
+			Category furniture = new Category("Furniture", "Furniture and Furniture");
+			Category book = new Category("Book", "bookstore and library products");
+			categoryRepository.save(furniture);
+			categoryRepository.save(book);
+			productRepository.save(new ProductModel("A234","House1", 12, furniture));
+			productRepository.save(new ProductModel("C234","House3", 56,furniture));
+			productRepository.save(new ProductModel("D3453","Home Food", 72, book));
 			
 		};
 	}
