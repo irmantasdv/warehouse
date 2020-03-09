d9163341f48ab3929cb1b2a351576074ddf84f34
irmantasdv <irmantas.dv@gmail.com>
2020-03-09 19:12:31 +0200

    Created Category GET, CREATE, UPDATE service

--- SpringApp-warehouse/src/main/java/com/springReact/warehouse/service/CategoryService.java
+++ SpringApp-warehouse/src/main/java/com/springReact/warehouse/service/CategoryService.java
@@ -2,10 +2,21 @@ package com.springReact.warehouse.service;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+
 import com.springReact.warehouse.model.Category;
+import com.springReact.warehouse.model.ProductModel;
 
 public interface CategoryService {
 
-	List<Category> gelAllCategories();
+	List<Category> getAllCategories();
+
+	ResponseEntity<Category> getCategoryproducts(Long id);
+
+	ResponseEntity<Category> addProductToCategory(Long id, ProductModel product);
+
+	ResponseEntity<Category> createCategory(Category category);
+	
+	ResponseEntity<Category> updateCategory(Long id, Category category);
 
 }
