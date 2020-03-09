beb50233e9dd5b992ef1eb977cd2706a82d3afbb
irmantasdv <irmantas.dv@gmail.com>
2020-03-09 19:16:55 +0200

    implementing Category GET POST UPDATE and created Category controler

--- SpringApp-warehouse/src/main/java/com/springReact/warehouse/controller/CategoryController.java
+++ SpringApp-warehouse/src/main/java/com/springReact/warehouse/controller/CategoryController.java
@@ -3,10 +3,17 @@ package com.springReact.warehouse.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springReact.warehouse.model.Category;
+import com.springReact.warehouse.model.ProductModel;
 import com.springReact.warehouse.service.CategoryService;
 
 @RestController
@@ -18,7 +25,27 @@ public class CategoryController {
 	@GetMapping(path = "/categories")
 	public List<Category> returnAllCategories() {
 
-		return categoryService.gelAllCategories();
+		return categoryService.getAllCategories();
 	}
 
+	@GetMapping(path = "/categories/{id}/products")
+	public ResponseEntity<Category> returnCategoryProducts(@PathVariable Long id) {
+
+		return categoryService.getCategoryproducts(id);
+	}
+
+//	@PutMapping("/categories/{id}/products")
+//	public ResponseEntity<Category> updateProductModel(@RequestParam Long id, @RequestBody ProductModel product) {
+//		return categoryService.addProductToCategory(id, product);
+//	}
+	@PostMapping("/categories/category")
+	public ResponseEntity<Category> postCategory(Category category) {
+		return categoryService.createCategory(category);
+	}
+
+	@PutMapping("/categories/category/{id}")
+	public ResponseEntity<Category> createProductInCategory(@PathVariable Long id,@RequestBody Category category) {
+
+		return categoryService.updateCategory(id, category);
+	}
 }
--- SpringApp-warehouse/src/main/java/com/springReact/warehouse/service/CategoryServiceImplementation.java
+++ SpringApp-warehouse/src/main/java/com/springReact/warehouse/service/CategoryServiceImplementation.java
@@ -1,11 +1,17 @@
 package com.springReact.warehouse.service;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.springReact.warehouse.model.Category;
+import com.springReact.warehouse.model.ProductModel;
 import com.springReact.warehouse.repository.CategoryRepository;
+import com.springReact.warehouse.repository.ProductRepository;
 
 @Service
 public class CategoryServiceImplementation implements CategoryService {
@@ -14,9 +20,47 @@ public class CategoryServiceImplementation implements CategoryService {
 	private CategoryRepository cetegoryRepository;
 
 	@Override
-	public List<Category> gelAllCategories() {
+	public List<Category> getAllCategories() {
 
 		return cetegoryRepository.findAll();
+	}
+
+	@Override
+	public ResponseEntity<Category> getCategoryproducts(Long id) {
+
+		return cetegoryRepository.findById(id).map(record -> {
+			record.getProducts();
+			return ResponseEntity.ok().body(record);
+
+		}).orElse(ResponseEntity.notFound().build());
+	}
+
+	@Override
+	public ResponseEntity<Category> addProductToCategory(Long id, ProductModel product) {
+
+		return cetegoryRepository.findById(id).map(record -> {
+			record.getProducts().add(product);
+			return ResponseEntity.ok().body(record);
+		}).orElse(ResponseEntity.notFound().build());
+	}
+
+	@Override
+	public ResponseEntity<Category> createCategory(Category category) {
+		cetegoryRepository.save(category);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(category.getId())
+				.toUri();
+		return ResponseEntity.created(uri).body(category);
+	}
+
+	@Override
+	public ResponseEntity<Category> updateCategory(Long id, Category category) {
+
+		return cetegoryRepository.findById(id).map(record -> {
+			record.setName(category.getName());
+			record.setDecription(category.getDecription());
+			Category updated = cetegoryRepository.save(record);
+			return ResponseEntity.ok().body(updated);
+		}).orElse(ResponseEntity.notFound().build());
 	}
 
 }
